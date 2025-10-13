f9ae2934bbb7228bf14dab9fa9dad9530645b221
Micim987 <4733781+Micim987@users.noreply.github.com>
2025-10-12 23:49:30 -0400

    Enhance Honda Car Interface: Integrate CarInterfaceExt and add CarControllerParamsExt class for extended parameters

---
+++ opendbc/sunnypilot/car/honda/interface_ext.py
@@ -0,0 +1,22 @@
+
+from opendbc.car import structs
+from opendbc.car.honda.values import CarControllerParams, HONDA_BOSCH
+from opendbc.sunnypilot.car.honda.values_ext import CarControllerParamsExt
+import numpy as np
+
+class CarInterfaceExt:
+  def __init__(self, CP: structs.CarParams, CP_SP: structs.CarParamsSP):
+    self.CP = CP
+    self.CP_SP = CP_SP
+
+  def get_pid_accel_limits(self, CP, current_speed, cruise_speed):
+    if self.CP.carFingerprint in HONDA_BOSCH:
+      return CarControllerParams.BOSCH_ACCEL_MIN, CarControllerParams.BOSCH_ACCEL_MAX
+    elif self.CP.carFingerprint not in HONDA_BOSCH and self.CP_SP.enableGasInterceptor:
+      return CarControllerParamsExt.NIDEC_PEDAL_ACCEL_MIN, CarControllerParamsExt.NIDEC_PEDAL_ACCEL_MAX
+    else:
+      # NIDECs don't allow acceleration near cruise_speed,
+      # so limit limits of pid to prevent windup
+      ACCEL_MAX_VALS = [CarControllerParams.NIDEC_ACCEL_MAX, 0.2]
+      ACCEL_MAX_BP = [cruise_speed - 2., cruise_speed - .2]
+      return CarControllerParams.NIDEC_ACCEL_MIN, np.interp(current_speed, ACCEL_MAX_BP, ACCEL_MAX_VALS)
--- opendbc/sunnypilot/car/honda/values_ext.py
+++ opendbc/sunnypilot/car/honda/values_ext.py
@@ -12,6 +12,10 @@ class HondaFlagsSP(IntFlag):
   CLARITY = 1
   EPS_MODIFIED = 2
 
+class CarControllerParamsExt:
+  NIDEC_PEDAL_ACCEL_MIN = -4.0
+  NIDEC_PEDAL_ACCEL_MAX = 2.0
+
 
 class HondaSafetyFlagsSP:
   CLARITY = 1
